14b146b30ebf06cdadfd5ae009a20e69c1bcb154
Asuka Shinohara <dryerrr@gmail.com>
2014-01-23 13:04:26 +0900

    だいたいのレイアウトが決まった。投票機能をつくってみたが、まだうまくなっていない。

--- vote/ViewController.h
+++ vote/ViewController.h
@@ -10,4 +10,37 @@
 
 @interface ViewController : UIViewController
 
+{
+    UILabel *label;
+    UILabel *label1;
+    UILabel *label2;
+    UILabel *label3;
+    UILabel *label4;
+    UILabel *label5;
+    UIDatePicker *datepicker;
+    int counter1;
+    int counter2;
+    int counter3;
+    int counter4;
+    int counter5;
+}
+
+@property (nonatomic, retain) IBOutlet UILabel *label;
+@property (nonatomic, retain) IBOutlet UIDatePicker *datepicker;
+
+@property (nonatomic, retain) IBOutlet UILabel *label1;
+@property (nonatomic, retain) IBOutlet UILabel *label2;
+@property (nonatomic, retain) IBOutlet UILabel *label3;
+@property (nonatomic, retain) IBOutlet UILabel *label4;
+@property (nonatomic, retain) IBOutlet UILabel *label5;
+
+@property (nonatomic, retain) IBOutlet UILabel *count1;
+@property (nonatomic, retain) IBOutlet UILabel *count2;
+@property (nonatomic, retain) IBOutlet UILabel *count3;
+@property (nonatomic, retain) IBOutlet UILabel *count4;
+@property (nonatomic, retain) IBOutlet UILabel *count5;
+
+- (IBAction)DateChanged:(id)sender;
+- (IBAction)Vote:(id)sender;
+
 @end
--- vote/ViewController.m
+++ vote/ViewController.m
@@ -14,10 +14,75 @@
 
 @implementation ViewController
 
+
+@synthesize label, datepicker;
+
+- (IBAction)DateChanged:(id)sender{
+    
+    //ラベルに表示する日付・時刻のフォーマットを指定
+    NSDateFormatter *df = [[NSDateFormatter alloc]init];
+    df.dateFormat = @"MM月dd日 HH時mm分";
+    
+    //ラベルに指定したフォーマットで表示
+    label.text = [df stringFromDate:datepicker.date];
+    
+}
+
+- (IBAction)Vote:(id)sender{
+    
+    //ラベルに表示する日付・時刻のフォーマットを指定
+    NSDateFormatter *df = [[NSDateFormatter alloc]init];
+    df.dateFormat = @"MM月dd日 HH時mm分";
+    
+    //ラベルに指定したフォーマットで表示
+    label.text = [df stringFromDate:datepicker.date];
+    
+    if (counter1 >= 1) {
+        if ([label.text isEqualToString:label1.text]) {
+            counter1 = counter1 +1;
+            _count1.text = [NSString stringWithFormat:@"%d票",counter1];
+        }
+    } else {
+        counter1 = 1;
+        label1.text = label.text;
+        _count1.text = [NSString stringWithFormat:@"%d票",counter1];
+    }
+    
+    
+    
+    if (counter2 >= 1) {
+        if ([label.text isEqualToString:label2.text]) {
+            counter2 = counter2 +1;
+            _count2.text = [NSString stringWithFormat:@"%d票",counter2];
+        }
+    } else {
+        counter2 = 1;
+        label2.text = label.text;
+        _count2.text = [NSString stringWithFormat:@"%d票",counter2];
+    }
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    datepicker.minuteInterval = 30;
+    
 }
 
 - (void)didReceiveMemoryWarning
